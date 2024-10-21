efb74ef541db512627ad3a2a02ca9999c3341007
Arlan <arland3v@gmail.com>
2024-10-21 16:03:45 -0300

    correção

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,21 @@
+import migrationRunner from 'node-pg-migrate'
+import { join } from 'node:path'
+
+export default async function migrations(request, response) {
+  if (request.method === 'GET') {
+    console.log('Entrou no GET')
+  }
+  if (request.method === 'POST') {
+    console.log('Entrou no POST')
+  }
+
+  const migration = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join('infra', 'migrations'),
+    direction: 'up',
+    verbose: true,
+    migrationsTable: 'pgmigrations',
+  })
+  response.status(200).json(migration)
+}
