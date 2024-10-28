4e2562e78993a2074b6435d6d22e78d9b20ec337
Arlan <arland3v@gmail.com>
2024-10-28 14:01:49 -0300

    Upload: Config.jest and config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
-const nextJest = require('next/jest')
+const dotenv = require ("dotenv")
+dotenv.config({
+    path: '.env.development',
+})
+const nextJest = require("next/jest")
 
+const createJestconfig = nextJest({
+    dir: '.'
+})
+const jestConfig = createJestconfig({
+    moduleDirectories: ["node_modules", "<rootDir>"],
+})
 module.exports = jestConfig
